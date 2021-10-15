1f7a70e607b4beb4847dd9951825644335f425f0
OKAWA TOMOKI <okawa.tomo.441@gmail.com>
2021-10-15 17:17:11 +0900

    Initial Commit

--- app/controllers/relaxes_controller.rb
+++ app/controllers/relaxes_controller.rb
@@ -1,69 +1,71 @@
 class RelaxesController < ApplicationController
 
-    def index
-        if params[:search] == nil
-            @relaxes= Relax.all.page(params[:page]).per(3).order(id: "DESC")
-        elsif params[:search] == ''
-            @relaxes= Relax.all.page(params[:page]).per(3).order(id: "DESC")
-        else
-            #部分検索
-            @relaxes = Relax.where("body LIKE ? ",'%' + params[:search] + '%').page(params[:page]).per(3).order(id: "DESC")
-            
-        end
-        @relax = Relax.new
-    end
-
-    def new
-        @relax = Relax.new
-    end
-
-    def create
-        relax = Relax.new(relax_params)
-        relax.user_id = current_user.id
-            if relax.save
-            redirect_to :action => "index"
-            else
-            redirect_to :action => "new"
-            end
-    end
-
-    def show
-        @relax = Relax.find(params[:id])
-        @comments = @relax.comments
-        @comment = Comment.new
-    end
-
-    def edit
-        @relax = Relax.find(params[:id])
-    end
-
-    def update
-        relax = Relax.find(params[:id])
-        if relax.update(relax_params)
-            redirect_to :action => "show", :id => relax.id
-        else
-            redirect_to :action => "new"
-        end
-    end
-
-
-    def destroy
-        relax = Relax.find(params[:id])
-        relax.destroy
-        redirect_to action: :index
-    end
-
-    def cards
-      if params[:search] == nil
-        @relaxes= Relax.all.page(params[:page]).per(12).order(id: "DESC")
+  def index
+    if params[:search] == nil
+      @relaxes= Relax.all.page(params[:page]).per(3).order(id: "DESC")
     elsif params[:search] == ''
-        @relaxes= Relax.all.page(params[:page]).per(12).order(id: "DESC")
+      @relaxes= Relax.all.page(params[:page]).per(3).order(id: "DESC")
     else
-        #部分検索
-        @relaxes = Relax.where("body LIKE ? ",'%' + params[:search] + '%').page(params[:page]).per(12).order(id: "DESC")
-        
+      #部分検索
+      @relaxes = Relax.where("body LIKE ? ",'%' + params[:search] + '%').page(params[:page]).per(3).order(id: "DESC")
+      
     end
     @relax = Relax.new
+  end
+
+  def new
+    @relax = Relax.new
+  end
+
+  def create
+    relax = Relax.new(relax_params)
+    relax.user_id = current_user.id
+      if relax.save
+      redirect_to :action => "index"
+      else
+      redirect_to :action => "new"
+      end
+  end
+
+  def show
+    @relax = Relax.find(params[:id])
+    @comments = @relax.comments
+    @comment = Comment.new
+  end
+
+  def edit
+    @relax = Relax.find(params[:id])
+  end
+
+  def update
+    relax = Relax.find(params[:id])
+    if relax.update(relax_params)
+      redirect_to :action => "show", :id => relax.id
+    else
+      redirect_to :action => "new"
+    end
+  end
+
+
+  def destroy
+    relax = Relax.find(params[:id])
+    relax.destroy
+    redirect_to action: :index
+  end
+
+  def cards
+    if params[:search] == nil
+      @relaxes= Relax.all.page(params[:page]).per(12).order(id: "DESC")
+    elsif params[:search] == ''
+      @relaxes= Relax.all.page(params[:page]).per(12).order(id: "DESC")
+    else
+      #部分検索
+      @relaxes = Relax.where("body LIKE ? ",'%' + params[:search] + '%').page(params[:page]).per(12).order(id: "DESC")
+      
+  end
+
+  
+  @relax = Relax.new
     end
 
     private
